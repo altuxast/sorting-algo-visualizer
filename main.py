0175785677d6fb2101a391d407deee915adee9d9
altuxast <drcageorge@gmail.com>
2023-08-30 13:22:32 +1000

    Generating lists

--- main.py
+++ main.py
@@ -2,3 +2,32 @@ import pygame
 import random
 import math
 pygame.init()
+
+class DrawInformation:
+    BLACK = 0, 0, 0
+    BLUE = 0, 0, 255
+    GREEN = 0, 255, 0
+    GREY = 128, 128, 128
+    RED = 255, 0, 0
+    WHITE = 255, 255, 255
+    BACKGROUND_COLOR = WHITE
+
+    SIDE_PAD = 100
+    TOP_PAD = 150
+
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Visualization")
+        self.set_list(lst)
+
+    def set_list(self, lst):
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        self.block_height = math.floor((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
